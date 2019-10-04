b1f71e295d28859fbc83b90a3b552ec76b3922d1
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-04 09:17:41 +0700

    Dibuat : Jumat,4 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan18/kelipatan/PBO11K10118901Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan18.kelipatan;
+
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Tentang Program  : Menghitung kelipatan 3.5
+ */
+public class PBO11K10118901Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i = 3.5;
+        while (i<=35) {
+            System.out.println(i);
+            i = i + 3.5;
+        }
+    }
+    
+}
